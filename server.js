3fa36862b87996451529cd8f5a18b24aa5e2ad00
kamranikramofficial <kamranikramofficial@gmail.com>
2026-03-25 17:18:48 +0500

    fix server

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const corsOptions = {
         'http://localhost:5173',
         'http://localhost:5174',
         'http://localhost:5175',
-        'https://ai-clinic-pro.vercel.app/'
+        'https://ai-clinic-pro.vercel.app'
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
